8b019739abc9293ce9dbaf30fca3d5fb9335ff38
loveward <loveward94@gmail.com>
2019-11-11 14:29:15 +0800

    fix: 修复home页面 会输出配置信息的bug(怎么想这么做都不合适)

--- app/controller/home.js
+++ app/controller/home.js
@@ -6,7 +6,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     // const { ctx } = this;
-    this.ctx.body = this.app.pgsql.config;
+    this.ctx.body = "<p>启动成功</p>";
     this.ctx.status = 200;
   }
 }
